290317700177b5defdc1e9f8bc705ae297cd81f5
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-06-13 11:58:25 +0200

    Méthode router->exports

--- controllers/barsController.js
+++ controllers/barsController.js
@@ -2,19 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Bar = require("../models/bars");
 
-// Ajouter un bar
-router.post("/bars", async (req, res) => {});
+exports.getAllBars = (req, res) => {};
 
-// Modifier un bar
-router.put("/bars/:id", async (req, res) => {});
+exports.getBarById = (req, res) => {};
 
-// Supprimer un bar
-router.delete("/bars/:id", async (req, res) => {});
+exports.createBar = (req, res) => {};
 
-// Récupérer tous les bars
-router.get("/bars", async (req, res) => {});
+exports.updateBar = (req, res) => {};
 
-// Récupérer un bar
-router.get("/bars/:id", async (req, res) => {});
+exports.deleteBar = (req, res) => {};
 
 module.exports = router;
