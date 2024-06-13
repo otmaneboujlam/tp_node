e7aed6b27bb2c53a3d95cb2419a4396bb54c7673
Clement Olivier <oliclems@gmail.com>
2024-06-13 13:15:00 +0200

    feat(biereCommande): add controller

---
+++ controllers/biere_commandeController.js
@@ -0,0 +1,50 @@
+const { BiereCommande } = require("../models/models");
+
+const addBiereToCommande = async (req, res) => {
+  try {
+    const idBiere = parseInt(req.params.id_biere, 10);
+    const idCommande = parseInt(req.params.id_commande, 10);
+
+    if (!Number.isInteger(idBiere) || !Number.isInteger(idCommande)) {
+      return res
+        .status(400)
+        .json({ error: "Incorrect, les ids doivent être des nombres." });
+    }
+
+    const biereCommande = await BiereCommande.create({
+      biere_id: idBiere,
+      commande_id: idCommande,
+    });
+    res.status(201).json(biereCommande);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const removeBiereFromCommande = async (req, res) => {
+  try {
+    const idBiere = parseInt(req.params.id_biere, 10);
+    const idCommande = parseInt(req.params.id_commande, 10);
+
+    if (!Number.isInteger(idBiere) || !Number.isInteger(idCommande)) {
+      return res
+        .status(400)
+        .json({ error: "Incorrect, les ids doivent être des nombres." });
+    }
+
+    await BiereCommande.destroy({
+      where: {
+        biere_id: idBiere,
+        commande_id: idCommande,
+      },
+    });
+    res.status(200).send();
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  addBiereToCommande,
+  removeBiereFromCommande
+};
