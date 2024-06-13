1e58bb65963ec432840d329a1a7b2514a905ded2
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-06-13 13:53:27 +0200

    Logique des fonctions

--- controllers/barsController.js
+++ controllers/barsController.js
@@ -2,14 +2,86 @@ const express = require("express");
 const router = express.Router();
 const Bar = require("../models/bars");
 
-exports.getAllBars = (req, res) => {};
+exports.getAllBars = (req, res) => {
+  Bar.findAll()
+    .then((bars) => {
+      res.json(bars);
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error.message });
+    });
+};
 
-exports.getBarById = (req, res) => {};
+exports.getBarById = (req, res) => {
+  const id = req.params.id;
+  Bar.findByPk(id)
+    .then((bar) => {
+      if (bar === null) {
+        res.status(404).json({ error: "Bar not found" });
+      } else {
+        res.json(bar);
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error.message });
+    });
+};
 
-exports.createBar = (req, res) => {};
+exports.createBar = (req, res) => {
+  const { name, address, tel, email, description } = req.body;
+  Bar.create({
+    name: name,
+    address: address,
+    tel: tel,
+    email: email,
+    description: description,
+  })
+    .then((bar) => {
+      res.status(201).json(bar);
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error.message });
+    });
+};
 
-exports.updateBar = (req, res) => {};
+exports.updateBar = (req, res) => {
+  const id = req.params.id;
+  const { name, address, tel, email, description } = req.body;
+  Bar.update(
+    {
+      name: name,
+      address: address,
+      tel: tel,
+      email: email,
+      description: description,
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  )
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error.message });
+    });
+};
 
-exports.deleteBar = (req, res) => {};
+exports.deleteBar = (req, res) => {
+  const id = req.params.id;
+  Bar.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error.message });
+    });
+};
 
 module.exports = router;
