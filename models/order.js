c64989d4c878aad54f78a4ac65e13970d3ce9a31
Otmane Boujlam <otmane.boujlam@gmail.com>
2024-06-13 11:24:32 +0200

    fix : order model

--- models/order.js
+++ models/order.js
@@ -1,13 +1,17 @@
 const sequelize = require("sequelize")
 const db = require("../config/database")
+const Bar = require('./bars');
 
 const Order = db.define('Order', {
     id : { type : sequelize.INTEGER, primaryKey : true, autoIncrement : true },
     name : { type : sequelize.STRING },
     price : { type : sequelize.FLOAT, validate : { min : 0 } },
     date : { type : sequelize.DATE },
-    statut : { type : sequelize.ENUM('en cours', 'terminée') },
-    bars_id : { type : sequelize.INTEGER, allowNull : false},
+    statut : { type : sequelize.ENUM, values: ["en cours", "terminée"] },
+    bars_id : { type : sequelize.INTEGER, references: {
+        model: Bar,
+        key: "id",
+      }},
 })
 
 module.exports = Order
